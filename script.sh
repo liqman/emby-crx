9b394dbda5a977e6149746ecb693039761cb99ec
liqman <48323002+liqman@users.noreply.github.com>
2025-02-15 00:37:14 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ wget https://raw.githubusercontent.com/liqman/emby-crx/refs/heads/master/static/
 wget https://raw.githubusercontent.com/liqman/emby-crx/refs/heads/master/static/js/common-utils.js -P emby-crx/
 wget https://raw.githubusercontent.com/liqman/emby-crx/refs/heads/master/static/js/jquery-3.6.0.min.js -P emby-crx/
 wget https://raw.githubusercontent.com/liqman/emby-crx/refs/heads/master/static/js/md5.min.js -P emby-crx/
-wgethttps://raw.githubusercontent.com/liqman/emby-crx/refs/heads/master/content/main.js -P emby-crx/
+wget https://raw.githubusercontent.com/liqman/emby-crx/refs/heads/master/content/main.js -P emby-crx/
 wget https://raw.githubusercontent.com/liqman/emby-crx/refs/heads/master/content/config.js -P emby-crx/
 
 # 读取index.html文件内容
